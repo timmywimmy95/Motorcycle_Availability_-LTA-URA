8cd27c9a7c9cab260584c940ea0d17fba9fa3d00
TimmyWimmy <timothykwok95@gmail.com>
2023-02-12 10:45:18 +0800

    changed geocode url to reflect netlify url

--- src/Components/GMapGeoCode.js
+++ src/Components/GMapGeoCode.js
@@ -21,7 +21,7 @@ const GMapGeoCode = ({
     const config = {
       method: 'get',
       maxBodyLength: Infinity,
-      url: `http://localhost:3001/search`,
+      url: `https://motorcycle-carpark-availability-sg.netlify.app/3001/search`,
       params: { address: userInput },
     };
 
